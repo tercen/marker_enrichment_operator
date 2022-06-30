73e82793f086496bd9817fab257cd4f30107e573
agouy <contact@agouy.com>
2022-06-30 08:53:22 +0000

    first version

--- main.R
+++ main.R
@@ -1,11 +1,31 @@
 library(tercen)
-library(dplyr, warn.conflicts = FALSE)
+library(dplyr)
 
-ctx = tercenCtx()
+options(dplyr.summarise.inform = FALSE)
 
-ctx %>%
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
+get.score <- function(df_in) {
+  global_median <- median(df_in$.y, na.rm = TRUE)
+  df_out <- df_in %>% 
+    group_by(.ri, .ci) %>%
+    summarise(
+      col_median = median(.y, na.rm = TRUE),
+      col_mad = median(abs(.y - col_median), na.rm = TRUE),
+      global_mad = median(abs(.y - global_median), na.rm = TRUE),
+      global_median = global_median,
+      score = 0.6745 * (col_median - global_median) / col_mad # not a magic number
+    ) %>% 
+    select(.ri, .ci, score) %>%
+    mutate(
+      p_value = 2 * pnorm(abs(score), lower.tail = FALSE),
+      neglog_p_value = -log10(p_value)
+    ) %>% 
+    ungroup()
+  return(df_out)
+}
+
+(ctx = tercenCtx()) %>%
+  select(.ci, .ri, .y) %>%
+  group_by(.ri) %>%
+  do(get.score(.)) %>%
   ctx$addNamespace() %>%
   ctx$save()
