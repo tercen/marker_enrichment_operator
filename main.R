f9ed0cc48835ccbfa4d9562ceda5bf9104b5ad28
agouy <contact@agouy.com>
2022-07-07 12:53:13 +0000

    fixed issue with single point cells

--- main.R
+++ main.R
@@ -5,18 +5,21 @@ options(dplyr.summarise.inform = FALSE)
 
 get.score <- function(df_in) {
   global_median <- median(df_in$.y, na.rm = TRUE)
+  global_mad <- median(abs(df_in$.y - global_median), na.rm = TRUE)
   df_out <- df_in %>% 
     group_by(.ri, .ci) %>%
     summarise(
       col_median = median(.y, na.rm = TRUE),
       col_mad = median(abs(.y - col_median), na.rm = TRUE),
-      global_mad = median(abs(.y - global_median), na.rm = TRUE),
+      global_mad = global_mad,
       global_median = global_median,
-      score = 0.6745 * (col_median - global_median) / col_mad # not a magic number
+      n = n(),
     ) %>%
-    ungroup() %>%
-    mutate(scl = mean(range(score))) %>%
-    mutate(score = score - scl) %>%
+    mutate(score = if_else(
+      n < 10,
+      0.6745 * (col_median - global_median) / global_mad, # not a magic number
+      0.6745 * (col_median - global_median) / col_mad
+    )) %>%
     select(.ri, .ci, score) %>%
     mutate(
       p_value = 2 * pnorm(abs(score), lower.tail = FALSE),
