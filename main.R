f7dd5db92f024e0b6ed1976bff5720f09c29b56a
agouy <contact@agouy.com>
2022-11-02 16:35:37 +0000

    added score threshold

--- main.R
+++ main.R
@@ -9,7 +9,10 @@ get.score <- function(df_in) {
   df_out <- df_in %>% 
     group_by(.ri, .ci) %>%
     summarise(
-      col_median = median(.y, na.rm = TRUE),
+      col_median = {
+        med = median(.y, na.rm = TRUE)
+        if_else(med == 0, min(.y[.y > 0]), med)
+      },
       col_mad = median(abs(.y - col_median), na.rm = TRUE),
       global_mad = global_mad,
       global_median = global_median,
@@ -22,6 +25,12 @@ get.score <- function(df_in) {
     )) %>%
     select(.ri, .ci, score) %>%
     mutate(
+      score = if_else(score > 20, 20, score)
+    ) %>% 
+    mutate(
+      score = if_else(score < -20, -20, score)
+    ) %>% 
+    mutate(
       p_value = 2 * pnorm(abs(score), lower.tail = FALSE),
       neglog_p_value = -log10(p_value)
     ) %>% 
@@ -30,9 +39,24 @@ get.score <- function(df_in) {
 }
 
 ctx <- tercenCtx()
-ctx %>%
+df <- ctx %>%
   select(.ci, .ri, .y) %>%
   group_by(.ri) %>%
   do(get.score(.)) %>%
   ctx$addNamespace() %>%
   ctx$save()
+
+df_in <- ctx %>%
+  select(.ci, .ri, .y) %>%
+  filter(.ri == 5)
+
+
+df_in %>% 
+  filter(.ci == 2) %>%
+  summarise(
+
+    col_mad = median(abs(.y - col_median), na.rm = TRUE),
+    global_mad = global_mad,
+    global_median = global_median,
+    n = n(),
+  )
